48fd27d66c717b87ff5b5d20820d78f589bb55d3
hammer-hao <103453499+hammer-hao@users.noreply.github.com>
2022-11-06 22:46:21 +0000

    implemented retry module to eliminate bad requests

--- API requests/getladderinfo.py
+++ API requests/getladderinfo.py
@@ -19,13 +19,21 @@ from joblib import Parallel, delayed # for running parallel requests
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 
+#import the retry module that allows us to resend requests and skip bad requests if necessary
+mrequest = requests.Session()
+retry = Retry(connect=3, backoff_factor=1)
+adapter = HTTPAdapter(max_retries=retry)
+mrequest.mount('http://', adapter)
+mrequest.mount('https://', adapter)
+
+
 #11.6.2022: Note that access tokens are temporary and need to be changed to run the code by logging into the battle.net account.
 #Creating dictionaries to convert battle.net terms to english
 region_id = {"us":1,
              "eu":2}
 ladder_id = {0:"Bronze", 1:"Silver", 2:"Gold", 3:"Platinum",
              4:"Diamond", 5:"Masters", 6:"Grandmaster"}
-token = {"access_token":"EUPsQbMXte0hp3FT0oVdyu3qOBEwlGofBR"}
+token = {"access_token":"EUe7zpGALh34AbNXhfQg77CdgYI1TLxHyQ"}
 #####################################################
 
 #Defining a function that makes a API call to get ids for individual ladders given the league type
@@ -43,8 +51,8 @@ def getladder(season, region, leagueid, teamtype, queueid):
                   str(leagueid)
         )
     #save the response to league_response
-    league_response = requests.get(league_url, params={"locale": "en_US",
-                    "access_token": "EUPsQbMXte0hp3FT0oVdyu3qOBEwlGofBR"})
+    league_response = mrequest.get(league_url, params={"locale": "en_US",
+                    "access_token": "EUe7zpGALh34AbNXhfQg77CdgYI1TLxHyQ"})
     #Check if the response is 200 OK
     if "200" in str(league_response):
         print("request successful for " + region + " league " + str(leagueid) +
@@ -69,7 +77,9 @@ def update1v1ladder(region, season):
 #####################################################
 
 #By calling update1v1ladder() on different servers and different seasons, we can update the current standings
-test_ladder = update1v1ladder("eu", 52)
+eu_ladder = update1v1ladder("eu", 52)
+us_ladder = update1v1ladder("us", 52)
+kr_ladder = update1v1ladder("kr", 52)
 
 #now defining fromtiers_getladderid, which takes in a league (such as Bronze 1) and returns a list of ladder ids
 def fromtiers_getladderid(data_of_entire_ladder, leagueid, tier):
@@ -87,17 +97,42 @@ def fromtiers_getladderid(data_of_entire_ladder, leagueid, tier):
 ######################################################
 
 #now we run a for loop that loops through the ranks, and append the ladderid to ladderid_list
-ladderid_list = []
+us_ladderid_list = []
 for x in range(6):
     league_ladderid_list=[]
     for i in range(3): 
-        league_ladderid_list.append(fromtiers_getladderid(test_ladder, 
+        league_ladderid_list.append(fromtiers_getladderid(us_ladder, 
                                                           x, i+1))
-    ladderid_list.append(league_ladderid_list)
+    us_ladderid_list.append(league_ladderid_list)
     del league_ladderid_list
-ladderid_list.append(fromtiers_getladderid(test_ladder, 6, 1))
+#Finally, get the ladder ids in grandmaster, which has no tiers
+us_ladderid_list.append(fromtiers_getladderid(us_ladder, 6, 1))
 
-#now we have ladderid_list, which returns all ladderids within the server
+#repeat the process for eu and kr server
+
+#now we run a for loop that loops through the ranks, and append the ladderid to ladderid_list
+eu_ladderid_list = []
+for x in range(6):
+    league_ladderid_list=[]
+    for i in range(3): 
+        league_ladderid_list.append(fromtiers_getladderid(eu_ladder, 
+                                                          x, i+1))
+    eu_ladderid_list.append(league_ladderid_list)
+    del league_ladderid_list
+eu_ladderid_list.append(fromtiers_getladderid(eu_ladder, 6, 1))
+
+#now we run a for loop that loops through the ranks, and append the ladderid to ladderid_list
+kr_ladderid_list = []
+for x in range(6):
+    league_ladderid_list=[]
+    for i in range(3): 
+        league_ladderid_list.append(fromtiers_getladderid(kr_ladder, 
+                                                          x, i+1))
+    kr_ladderid_list.append(league_ladderid_list)
+    del league_ladderid_list
+kr_ladderid_list.append(fromtiers_getladderid(kr_ladder, 6, 1))
+
+#now we have region_ladderid_list, which returns all ladderids within the server
 ######################################################
 
 #to get player infos we need to know their profile ids
@@ -110,7 +145,7 @@ def fromladder_getplayers(ladderid, region):
                   "/"+
                   str(ladderid)
         )
-    ladder_response = requests.get(ladder_url, params=token)
+    ladder_response = mrequest.get(ladder_url, params=token)
     #Check if 200 OK response
     if "200" in str(ladder_response):
         print("request successful for ladderid = "+ str(ladderid))
@@ -132,40 +167,48 @@ def update_playerstats(listed_ladderid, region):
         for idx2, subdiv in enumerate(listed_ladderid[i]):
             division = (str(ladder_id[i]) + " " + str(idx2+1))
             #loop through each individual ladder (~100 players)
-            for ladder in subdiv:
-                #Call the fromladder_getplayers() function within the loop to get player details for individual ladder
-                members = fromladder_getplayers(ladder, region)
-                #Now loop through each player in that ladder, and create a list storing the details of each player
-                for member in members:
-                    playerstats = [member["character"]["displayName"],
-                                   member["character"]["id"], 
-                                   division,
-                                   member["favoriteRaceP1"]]
-                    #At the end, append the player's details to our list
-                    playerslist.append(playerstats)
-            del division
+            if type(subdiv)==list:
+                for ladder in subdiv:
+                    #Call the fromladder_getplayers() function within the loop to get player details for individual ladder
+                    members = fromladder_getplayers(ladder, region)
+                    #Now loop through each player in that ladder, and create a list storing the details of each player
+                    for member in members:
+                        try:
+                            playerstats = [member["character"]["displayName"],
+                                           member["character"]["id"], 
+                                           division,
+                                           member["favoriteRaceP1"]]
+                        #If one characteristic is not specified, ignore and move on
+                        except KeyError:
+                            pass
+                        #At the end, append the player's details to our list
+                        playerslist.append(playerstats)
+                del division
+            else:
+                print("subdiv does not exist for " + str(ladder_id(i)) + " tier " + str(idx2+1))
     #return the final list consisting of all players in all leagues
     return(playerslist)
 
 #Now by calling update_playerstats and specifying a list of ladders, we shall get a output of a list of all players
-test = update_playerstats(ladderid_list, "eu")
+#Note: Running the following part of code will make *Hundreds* of individual api calls
+us_players = update_playerstats(us_ladderid_list, "us")
+eu_players = update_playerstats(eu_ladderid_list, "eu")
+kr_players = update_playerstats(kr_ladderid_list, "eu")
 
 ######################################################
-#Saving player list to csv
 
-players_data=pd.DataFrame(test)
-players_data.to_csv("C:/Users/hammerhao/OneDrive/Desktop/DS105/playerslist_S51.csv")
+#Saving player list to csv, creating a seperate file for each ladder
+us_players_data = pd.DataFrame(us_players)
+eu_players_data = pd.DataFrame(eu_players)
+kr_players_data = pd.DataFrame(kr_players)
+us_players_data.to_csv("C:/Users/hammerhao/OneDrive/Desktop/DS105/us_players_S52.csv")
+eu_players_data.to_csv("C:/Users/hammerhao/OneDrive/Desktop/DS105/eu_players_S52.csv")
+kr_players_data.to_csv("C:/Users/hammerhao/OneDrive/Desktop/DS105/kr_players_S52.csv")
 
 #Now we have details of each player and their playerids, we will not make a seperate request for each player to get their match history
 #This will result in making tens of thousands of requests in total
 #To speed up the process, a parallel module is used here
 #We also do not want the code to terminate if there's error while requesting match history for one player
-#So we import the retry module that allows us to resend requests and skip bad requests if necessary
-mrequest = requests.Session()
-retry = Retry(connect=3, backoff_factor=1)
-adapter = HTTPAdapter(max_retries=retry)
-mrequest.mount('http://', adapter)
-mrequest.mount('https://', adapter)
 
 #Create a dictionary for indexing with playerids later
 players_dict={}
