9aa042af4fb5d53edcabc125e21aa0896198add4
hammer-hao <103453499+hammer-hao@users.noreply.github.com>
2022-11-09 15:13:34 +0000

    defined getmatchhistory()

--- API requests/sc2.py
+++ API requests/sc2.py
@@ -8,7 +8,8 @@ import requests
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 import ladders
-import references
+import players
+import APIkey
 
 #import the retry module that allows us to resend requests and skip bad requests if necessary
 mrequest = requests.Session()
@@ -64,6 +65,23 @@ def update1v1ladder(region, season):
         ladderlist.append(updatedladder)
     return(ladderlist)
 
+
+def formladderlist(ladder_response, region):
+    ladderid_list=[]
+    for idx, league in enumerate(ladder_response):
+        for idx2, div in enumerate(league):
+            try:
+                obj = div['division']
+                for ladder in obj:
+                    ladder_info = [ladder["ladder_id"],
+                                   APIkey.ladder_id[idx],
+                                   idx2+1,
+                                   region]
+                    ladderid_list.append(ladder_info)
+            except KeyError:
+                pass
+    return(ladderid_list)
+
 def fromtiers_getladderid(data_of_entire_ladder, leagueid, tier):
     tierdata = data_of_entire_ladder[leagueid][tier-1]
     if "division" in tierdata:
@@ -73,41 +91,18 @@ def fromtiers_getladderid(data_of_entire_ladder, leagueid, tier):
         return(tier_ladderidlist)
     #if can't fant the tier, print the error message:
     else:
-        print("divisions do not exist for " + str (references.ladder_id[leagueid]) + 
+        print("divisions do not exist for " + str (APIkey.ladder_id[leagueid]) + 
               str(tier))
         
-def update_playerstats(listed_ladderid, region):
-    playerslist=[]
-    #Loop through all the leagues
-    for i in range(6):
-        #loop through each division in the league and 
-        for idx2, subdiv in enumerate(listed_ladderid[i]):
-            division = (str(ladder_id[i]) + " " + str(idx2+1))
-            #loop through each individual ladder (~100 players)
-            if type(subdiv)==list:
-                for indivladderid in subdiv:
-                    #Call the fromladder_getplayers() function within the loop to get player details for individual ladder
-                    obj = ladders.ladder(indivladderid, region)
-                    members = obj.fromladder_getplayers()
-                    #Now loop through each player in that ladder, and create a list storing the details of each player
-                    try:
-                        for member in members:
-                            try:
-                                playerstats = [member["character"]["displayName"],
-                                               member["character"]["id"], 
-                                               division,
-                                               member["favoriteRaceP1"],
-                                               member["character"]["realm"],
-                                               member["character"]["region"]]
-                            #If one characteristic is not specified, ignore and move on
-                            except KeyError:
-                                pass
-                            #At the end, append the player's details to our list
-                            playerslist.append(playerstats)
-                    except TypeError:
-                        pass
-                del division
-            else:
-                print("subdiv does not exist for " + str(references.ladder_id[i]) + " tier " + str(idx2+1))
-    #return the final list consisting of all players in all leagues
-    return(playerslist)
+def update_playerstats(ladderidlist):
+    playerlist = []
+    for ladder in ladderidlist:
+        thisladder = ladders.ladder(ladder[0], ladder[3], ladder[1], ladder[2])
+        thisplayerlist = thisladder.getplayers()
+        playerlist = playerlist + thisplayerlist
+    return playerlist
+
+def getmatchhistory(player_entry):
+    player = players.player(player_entry[0], player_entry[1], player_entry[5],
+                            player_entry[4], player_entry[3], player_entry[2])
+    return(player.getmatch())
