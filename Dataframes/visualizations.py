2dfba40fca9697aea566d8ad507bb763cc6c14cf
hammer-hao <103453499+hammer-hao@users.noreply.github.com>
2022-12-01 20:38:41 +0000

    Update visualizations.py

--- Dataframes/visualizations.py
+++ Dataframes/visualizations.py
@@ -60,6 +60,29 @@ figure2 = (ggplot(players_df, aes(x='totalgames')) +
            theme_bw()
            )
 
+#Graph 4: Qing's bar graph
+df = pd.DataFrame({
+    'Race':['PROTOSS','PROTOSS','TERRAN','TERRAN','ZERG','ZERG'],
+    'category':['mean PROTOSS','median PROTOSS','mean TERRAN','median TERRAN','mean ZERG','median ZERG'],
+    'Ratio':[1.13,1,1.09,1,1.16,1.03]
+})
+df['Race']=pd.Categorical(df['Race'],categories=['PROTOSS','TERRAN','ZERG'])
+df['category']=pd.Categorical(df['category'],categories=df['category'])
+print(df)
+
+dodge_text = position_dodge(width=0.9)
+
+figure2=(ggplot(df, aes(x='Race', y='Ratio', fill='category'))
+ + geom_col(stat='identity', position='dodge', show_legend=False)
+ + geom_text(aes(y=-.5, label='category'),
+             position=dodge_text,
+             color='gray', size=8, angle=45, va='top')
+ + geom_text(aes(label='Ratio'),
+             position=dodge_text,
+             size=8, va='bottom', format_string='{}%')
+ + lims(y=(0, 1.2))
+)
+
 #----------------------------Matches Datafram Visualizations-------------------------
 
 matches_full=pd.read_csv('processedmatches.csv')
