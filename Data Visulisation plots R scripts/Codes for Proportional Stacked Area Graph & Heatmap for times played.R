ef0f13fa8166f36b832f4a487b417b2d5ef18861
awoyu <117571397+awoyu@users.noreply.github.com>
2023-02-02 11:50:00 +0000

    Codes for Proportional Stacked Area Graph & Heatmap of times people play Starcraft 2

--- Data Visulisation plots R scripts/Codes for Proportional Stacked Area Graph & Heatmap for times played.R
+++ Data Visulisation plots R scripts/Codes for Proportional Stacked Area Graph & Heatmap for times played.R
@@ -1,4 +1,55 @@
+#CODE 1 = Proportional Stacked Area Graph
+
+#import files
+
+library(rvest)
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(datasets)
+library(reshape2)
+
+
+#set working directory and load files 
+setwd("~/DS105_Project")
+table_main2_file <- read.csv("processedplayers.txt")
+
+#creating different dataframes
+table_mmr_race_league <- table_main2_file[, c("mmr","race", "league")]
+table_mmr_race_region <- table_main2_file[, c("mmr","race", "region")]
+table_race_league <- table_main2_file[, c("race", "league")]
+
+
+#editing the format of the variables and creating a dataframe with the percentages
+
+table_race_league$league <- as.factor(table_race_league$league)
+table_race_league$race <- as.factor(table_race_league$race)
+view(table_race_league)
+
+main_table <- table_race_league %>% group_by(race, league) %>%
+  tally()
+
+main_final_table <- transform(main_table, percent = ave(n, league, FUN = prop.table))
+
+#creating the Proportional Stacked Area Graph
+
+main_final_table2 <- main_final_table[!(main_final_table$race=="Unknown" |main_final_table$race=="unknown"),]
+
+main_final_table2$league <- factor(main_final_table2$league , levels=c("Bronze 3", "Bronze 2", "Bronze 1", "Silver 3", "Silver 2", "Silve 1", "Gold 3", "Gold 2", "Gold 1", "Diamond 3", "Diamond 2", "Diamond 1", "Masters 3", "Masters 2", "Masters 1", "Grandmaster 1"))
+main_final_table2$race <- factor(main_final_table2$race , levels=c("RANDOM", "ZERG", "PROTOSS", "TERRAN"))
+
+final_graph_1 <- ggplot(main_final_table2, (aes(x = league,  y = percent, fill = race, group = race))) +
+  geom_area(alpha=0.6 , linewidth=0.3, colour="black") +   scale_fill_manual(values=c('grey', '#5519BD', '#FFFF40', '#244CB9'))
+
+#to view the graph
+final_graph_1
+
+#----------------------------------------------------------------------------------
+
+#CODE 2 = Heatmap of times people play Starcraft 2
+
 #import the packages
+
 library(rvest)
 library(tidyverse)
 library(dplyr)
@@ -16,11 +67,11 @@ matches_full <- read.csv("matchesdata.txt")
 matches_full_only_1v1 <- matches_full %>% filter(X7=="1v1")
 
 main_dataframe_matches <- matches_full_only_1v1 %>% 
-                              mutate(new_timezone=if_else(X4 == 1, 
+  mutate(new_timezone=if_else(X4 == 1, 
                               as.POSIXct(X10, origin="1970-01-01", timezone="America/New_York"), 
                               if_else(X4 == 2, 
                                       as.POSIXct(X10, origin="1970-01-01", tz="Europe/London"),
-                                              as.POSIXct(X10, origin="1970-01-01", tz="KR time" ))   ))
+                                      as.POSIXct(X10, origin="1970-01-01", tz="KR time" ))   ))
 
 
 #creating new coloumns (in 'main_dataframe_matches' dataframe) for day and hour
@@ -41,8 +92,8 @@ hr_labels <- c('00', '01', '02', '03', '04', '05', '06', '07', '08', '09',
                '20', '21', '22', '23', '24')
 
 main_table_heatmap$ordered_time = cut(main_table_heatmap$hour, breaks = hr_breaks, 
-             labels = hr_labels,
-             include.lowest = T, right = F)
+                                      labels = hr_labels,
+                                      include.lowest = T, right = F)
 
 
 main_table_heatmap$ordered_time <- factor(main_table_heatmap$ordered_time,
@@ -50,13 +101,13 @@ main_table_heatmap$ordered_time <- factor(main_table_heatmap$ordered_time,
                                                    '18', '17', '16', '15',
                                                    '14', '13', '12', '11', '10', '09',
                                                    '08', '07', '06', '05',
-                                                    '04', '03', '02', '01', '00'))
+                                                   '04', '03', '02', '01', '00'))
 
 
 main_table_heatmap$main_dataframe_matches.weekday <- factor(main_table_heatmap$main_dataframe_matches.weekday,
                                                             levels=c('Monday', 'Tuesday', 'Wednesday', 'Thursday',
                                                                      'Friday', 'Saturday', 'Sunday'))
-                                                                                      
+
 #final step is to create the Heatmap
 
 final_graph_heatmap_main <- ggplot(data= main_table_heatmap, aes(main_dataframe_matches.weekday, ordered_time, frame = main_dataframe_matches.X4)) + geom_bin2d() +  xlab(label = "weekday") + ylab(label = "hour") +
@@ -66,7 +117,8 @@ final_graph_heatmap_main <- ggplot(data= main_table_heatmap, aes(main_dataframe_
 #to view the graph
 
 final_graph_heatmap_main 
- 
+
+
 
 
 
